57a136ab513a2dd1dffd1f25c3d5b46f14ee5546
ACER <ACER@10.5.13.60>
2024-11-13 13:17:15 +0800

    ini projek

---
+++ src/main/java/com/mycompany/mi3c/Mi3c.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mi3c;
+
+/**
+ *
+ * @author ACER
+ */
+
+public class Mi3c 
+{
+ public static void main(String[] args) {
+    int a, b; 
+    a = 20; 
+    b = 30; 
+    if ( a > b) {
+    System.out.println ("nilai max adalah : " +(a)); 
+    }else {
+    System.out.println ("nilai max adalah : " +(b)); 
+    }
+    }
+}
